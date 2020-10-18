7b6a02f6de668bed7e740fe014334269f1943cdc
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-10-18 12:12:56 +0700

    done Java_BigInteger

---
+++ src/Java_BigInteger.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Java_BigInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger bi1 = new BigInteger(sc.next());
+        BigInteger bi2 = new BigInteger(sc.next());
+        sc.close();
+
+        BigInteger bi3, bi4;
+        bi3 = bi1.add(bi2);
+        bi4 = bi1.multiply(bi2);
+        System.out.println( bi3);
+        System.out.println( bi4);
+    }
+}
