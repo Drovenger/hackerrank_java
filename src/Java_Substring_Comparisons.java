0c75bb44a12104d27230981eadc6d5a3d42ff175
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-11-03 09:50:47 +0700

    done Java_Substring_Comparisons

--- src/Java_Substring_Comparisons.java
+++ src/Java_Substring_Comparisons.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
 public class Java_Substring_Comparisons {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = s.substring(0,k);
         String largest  = "";
@@ -11,14 +20,5 @@ public class Java_Substring_Comparisons {
             if(substring.compareTo(largest) >0) largest = substring;
         }
         return smallest + "\n" + largest;
-    }
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.next();
-        int k = scan.nextInt();
-        scan.close();
-
-        System.out.println(getSmallestAndLargest(s, k));
     }
 }
